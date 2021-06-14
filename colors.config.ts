4ad15fac060bbc8c4b51a017b3c2f5b722dc56f3
MKIsHereOficial <mkishere.oficial@gmail.com>
2021-06-14 15:00:38 -0300

    test homepage, colors config

---
+++ colors.config.ts
@@ -0,0 +1,11 @@
+export default {
+    "PRIMARY": "#f952d3",
+    "SECONDARY": "#5269f9",
+    "OK": '#3efa67',
+    "ERROR": '#faa63e',
+    '_vars': {
+        'SANITY': '#372294',
+        'HP': '#fa3e64',
+        'EXP_P': '#3e6afa'
+    }
+}
