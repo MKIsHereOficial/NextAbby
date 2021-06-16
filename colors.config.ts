d2b7781196908bb78b775b46fd31d5d979b41d51
MKIsHereOficial <mkishere.oficial@gmail.com>
2021-06-16 13:10:47 -0300

    typescript changes, new color palette, and new text variable

--- colors.config.ts
+++ colors.config.ts
@@ -1,6 +1,9 @@
 export default {
-    "PRIMARY": "#f952d3",
-    "SECONDARY": "#5269f9",
+    "BACKGROUND_PRIMARY": '#000',
+    "WHITE": '#fff',
+    "MAIN_COLOR": 'orange',
+    "PRIMARY": "#1c0d46",
+    "SECONDARY": "#271756",
     "OK": '#3efa67',
     "ERROR": '#faa63e',
     '_vars': {
---
+++ textVars.config.ts
@@ -0,0 +1,7 @@
+export default {
+    "LOGIN_ADVICE": `Olá!
+    
+    Parece que você não logou com o Discord!
+    
+    Assim que logar poderemos disponibilizar as informações do seu Personagem, ou criar um!`
+}
