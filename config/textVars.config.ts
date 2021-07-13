c4c6fb8a2ddad1abc5d5df7cd0dcd2deb32ae030
MKIsHereOficial <65752591+MKIsHereOficial@users.noreply.github.com>
2021-07-13 15:34:43 -0300

    refactor(Config): move to /config

---
+++ config/textVars.config.ts
@@ -0,0 +1,8 @@
+export default {
+    "LOGIN_ADVICE": `Olá!
+    
+    Parece que você não logou com o Discord!
+    
+    Assim que logar poderemos disponibilizar as informações do seu Personagem, ou criar um!`,
+    "COPYRIGHT": `Copyright 2021 GokuroHype`
+}
--- textVars.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    "LOGIN_ADVICE": `Olá!
-    
-    Parece que você não logou com o Discord!
-    
-    Assim que logar poderemos disponibilizar as informações do seu Personagem, ou criar um!`
-}
